1dc833ead30a26eb2d9d79d51b50c88103d048ac
firstclimbkazu <firstclimbkazu@gmail.com>
2017-08-01 12:42:39 +0000

    すこしだけ動的なページ追加

--- test/controllers/maps_controller_test.rb
+++ test/controllers/maps_controller_test.rb
@@ -1,14 +1,20 @@
 require 'test_helper'
 
 class MapsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "ClimbingMovieMap"
+  end
+  
   test "should get home" do
     get maps_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get maps_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
 end
